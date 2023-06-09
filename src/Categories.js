d3aaf7a0461e693070a3037c371432caf093af5e
O-T-i-A <sopromadzeotari@gmail.com>
2023-06-09 16:38:35 +0400

    imitial commit

---
+++ src/Categories.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Categories({ filterItems, categories }) {
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            key={index}
+            onClick={() => filterItems(category)}
+            className="filter-btn"
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Categories;
+/* <button className="filter-btn" onClick={() => filterItems("all")}>
+        all
+      </button> */
